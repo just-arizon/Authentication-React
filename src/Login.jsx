02da42a3022b007c877d7c09b789d9488d06e821
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-26 21:33:56 +0100

    Add access token

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,10 @@
 import {useRef, useState, useEffect, useContext} from 'react';
 import AuthContext from './context/AuthProvider';
+import axios from './api/axios'
+
+const LOGIN_URL = '/auth'
+
+
 
 const Login = () => {
     const { setAuth } = useContext(AuthContext);
@@ -21,10 +26,36 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         
-        console.log(user, pwd);
+        try {
+           const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd})), 
+           {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+           
+           };
+       console.log(JSON.stringify(response?.data));
+      
+       const accessToken = response?.data?.accessToken;
+       const roles = response?.data?.roles;
+       setAuth({ user, pwd, roles, accessToken});
+
         setUser('');
         setPwd('');
         setSuccess(true);
+
+        }catch (err) {
+          if(!err?.response){
+            setErrMsg('No Server Response')
+          }else if (err.response?.status === 400) {
+setErrMsg('Missing Username or Password')
+          } else if (err.response?.status === 401) {
+            setErrMsg('Unauthorized');
+        } else {
+            setErrMsg('Login Failed');
+        }
+        errRef.current.focus();
+        }
+   
     }
   return (
     <>
